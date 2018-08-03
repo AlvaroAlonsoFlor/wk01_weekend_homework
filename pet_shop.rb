abceacc00b1ba72cbbc8d7bd66a083f117b86d17
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-03 09:17:26 +0100

    Add third function

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,7 +1,14 @@
-def pet_shop_name(hash)
-  return hash[:name]
+def pet_shop_name(shop)
+  return shop[:name]
 end
 
-def total_cash(hash)
-  return hash[:admin][:total_cash]
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+# If we wanted to improve it we would pass another argument
+# saying add or remove and then the amount
+# that way you don't need to use negative numbers to remove
+def add_or_remove_cash(shop, amount)
+  return shop[:admin][:total_cash] += amount
 end
